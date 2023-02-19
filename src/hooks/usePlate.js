1cf0c9686e800b55032925e63d95908c07c7217c
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-02-19 19:59:06 +0100

    update

--- src/hooks/usePlate.js
+++ src/hooks/usePlate.js
@@ -48,7 +48,8 @@ const usePlate = () => {
             if (!state.playerIsOn) {
 
                 dispatch({ type: 'play', payload: true });
-                await tonearmControls.start({ rotate: 29, transition: { duration: 1.5, type: 'spring', damping: 9, onComplete: () => state.plate.audio.play() } });
+                await tonearmControls.start({ rotate: 29, transition: { duration: 1.5, type: 'spring', damping: 9 }});
+                await state.plate.audio.play();
                 tonearmControls.start({ rotate: [29.5, 29, 28.5, 29, 29.5], transition: { duration: 1, delay: .3, repeat: Infinity, ease: 'linear' } });
                 return plateControls.start({rotate: 360, transition: {duration: 2, repeat: Infinity, ease: 'linear'}});
             } else {
@@ -56,7 +57,7 @@ const usePlate = () => {
                 dispatch({ type: 'play', payload: false });
                 plateControls.start({ rotate: 0, transition: { duration: 1.5 } })
                 tonearmControls.start({ rotate: 0, transition: { duration: 1.3, ease: 'easeOut' } });
-                state.plate.audio.pause();
+                await state.plate.audio.pause();
                 return state.plate.audio.currentTime = 0;
             };
         };
