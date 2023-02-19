c46205e38ff326c1b9fe3926c724292fa53cc94c
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-02-19 23:20:10 +0100

    update

--- src/hooks/usePlate.js
+++ src/hooks/usePlate.js
@@ -42,23 +42,31 @@ const usePlate = () => {
         } else return;
     };
 
-    const play = async () => {
+    const anim = async () => {
+        await tonearmControls.start({ rotate: 29, transition: { duration: 1.5, type: 'spring', damping: 9 }});
+        tonearmControls.start({ rotate: [29.5, 29, 28.5, 29, 29.5], transition: { duration: 1, delay: .3, repeat: Infinity, ease: 'linear' } });
+        plateControls.start({rotate: 360, transition: {duration: 2, repeat: Infinity, ease: 'linear'}});
+        return;
+    }
+
+    const play =  async () => {
         if (!state.animationIsRunning) {
 
             if (!state.playerIsOn) {
 
                 dispatch({ type: 'play', payload: true });
-                await tonearmControls.start({ rotate: 29, transition: { duration: 1.5, type: 'spring', damping: 9 }});
-                await state.plate.audio.play();
-                tonearmControls.start({ rotate: [29.5, 29, 28.5, 29, 29.5], transition: { duration: 1, delay: .3, repeat: Infinity, ease: 'linear' } });
-                return plateControls.start({rotate: 360, transition: {duration: 2, repeat: Infinity, ease: 'linear'}});
+                await anim();
+                //tonearmControls.start({ rotate: 29, transition: { duration: 1.5, type: 'spring', damping: 9 }});
+                state.plate.audio.play();
+                //tonearmControls.start({ rotate: [29.5, 29, 28.5, 29, 29.5], transition: { duration: 1, delay: .3, repeat: Infinity, ease: 'linear' } });
+                //return plateControls.start({rotate: 360, transition: {duration: 2, repeat: Infinity, ease: 'linear'}});
 
             } else if (state.playerIsOn) {
 
                 dispatch({ type: 'play', payload: false });
-                plateControls.start({ rotate: 0, transition: { duration: 1.5 } })
-                tonearmControls.start({ rotate: 0, transition: { duration: 1.3, ease: 'easeOut' } });
-                await state.plate.audio.pause();
+                //plateControls.start({ rotate: 0, transition: { duration: 1.5 } })
+                //tonearmControls.start({ rotate: 0, transition: { duration: 1.3, ease: 'easeOut' } });
+                state.plate.audio.pause();
                 return state.plate.audio.currentTime = 0;
 
             };
